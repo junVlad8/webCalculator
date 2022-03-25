45cb6e4e79370bbc24dc515c9a5da0caac1ab8f9
junVlad8 <88398689+junVlad8@users.noreply.github.com>
2022-03-25 20:45:58 +0400

    v 1.3.3
    adding JS and changed HTML CSS

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use string';
+
+for ( let i = 0; i < buttons.length; i++ ) {
+  buttons[i].onclick = function () {
+    if ( content === '0' ) {
+      display.value = '';
+      content = '';
+
+      display.value += this.textContent;
+      content += this.textContent;
+    } else {
+  		display.value += this.textContent;
+  		content += this.textContent;
+    }
+  }
+}
+
+answer.onclick = function () {
+  display.value = eval ( content );
+  content = display.value;
+}
+
+del.onclick = function () {
+  display.value = '0';
+  content = '0';
+}
+
+sqrt.onclick = function () {
+  if ( content === '0' ) {
+    display.value = '';
+    content = '';
+
+    display.value += '√(';
+    content += 'Math.sqrt(';
+  } else {
+    display.value += '√(';
+    content += 'Math.sqrt(';
+  }
+}
